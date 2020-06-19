0b54d34ee4ce24de0a78eda0c759b4bd773fe597
DerZade <DerZade@gmail.com>
2020-06-20 01:21:10 +0200

    Fix typo

--- src/mvt/main.go
+++ src/mvt/main.go
@@ -71,7 +71,7 @@ func Run(flagSet *flag.FlagSet) {
 
 	// contour lines
 	timer = time.Now()
-	fmt.Println("▶️  Building countour lines")
+	fmt.Println("▶️  Building contour lines")
 	buildContours(path.Join(*inputPtr, "dem.asc.gz"), meta.ElevationOffset, &collections)
 	fmt.Println("✔️  Built contour lines in", time.Now().Sub(timer).String())
 
